29d8b9673ebb74b437f4deaae53fe093e3d36e0d
sivam93 <49158958+sivam93@users.noreply.github.com>
2019-06-04 10:57:35 +0530

    commit

--- hello.py
+++ hello.py
@@ -63,8 +63,9 @@ def hello():
     return operations.forgotpassword
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+   port = int(os.getenv('PORT', 5000))
+	print("Starting app on port %d" % port)
+	app.run(debug=True, port=port, host='0.0.0.0')
 
 
 
